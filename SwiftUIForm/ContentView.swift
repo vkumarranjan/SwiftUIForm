3f229c415bc6c6b1eb9d5ab0aa75ff981bdbb59c
Vinay Nation <vinay.raj.apple@gmail.com>
2021-06-16 20:38:51 +0530

    Added project

---
+++ SwiftUIForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIForm
+//
+//  Created by Vinay Nation on 16/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
