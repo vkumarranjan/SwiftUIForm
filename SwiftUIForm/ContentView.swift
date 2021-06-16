899198f10d341486e923c956cea34f4cbe2d2d49
Vinay Nation <vinay.raj.apple@gmail.com>
2021-06-16 22:00:42 +0530

    Added a small form into SwiftUI

--- SwiftUIForm/ContentView.swift
+++ SwiftUIForm/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fristName = ""
+    @State private var lastName = ""
+    @State private var birthdate = Date()
+    @State private var shouldSendNewsLetter = false
+    @State private var numberOfLikes = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            Form {
+                Section(header: Text("Personal Infromation")) {
+                    TextField("First Name", text: $fristName)
+                    TextField("Last Name", text: $lastName)
+                    DatePicker("Birthdate", selection: $birthdate, displayedComponents: .date)
+                }
+                
+                Section(header: Text("Actions")){
+                    Toggle("Send Newsletter", isOn: $shouldSendNewsLetter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of Likes", value: $numberOfLikes, in: 1...100)
+                    Text("This videos has \(numberOfLikes) likes")
+                }
+            }
+            .accentColor(.red)
+            .navigationTitle("Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        hideKeyboard()
+                    } label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                }
+            }
+            
+        }
+        
     }
 }
 
@@ -19,3 +53,12 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
